c5718c6462596ca3c7dfa51d7e2777597a470f2c
Milena <milena.hikari@profiz.com.br>
2025-03-12 10:19:24 -0300

    feat: add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'linebreak-style': ['error', 'unix'],
+      'indent': ['error', 2],
+    }
+  },
+  {files: ['**/*.js'], languageOptions: {sourceType: 'script'}},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
