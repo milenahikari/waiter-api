c25f6c6cbbfa1e5064da55a58c19790f0e7c10b5
Milena <milena.hikari@profiz.com.br>
2025-03-13 16:53:29 -0300

    feat: add mongoDB config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
-const port = 3001;
-const app = express();
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
+const uri =  `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.os34z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(uri).then(() => {
+  const port = 3001;
+  const app = express();
+
+  app.listen(port, () => {
+    console.log(`🚀 Server is running on http://localhost:${port}`);
+  });
+}).catch((error) => {
+  console.error('❌ Error connecting to MongoDB:', error);
 });
