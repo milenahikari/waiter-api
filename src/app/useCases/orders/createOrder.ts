72c2b8c8d3f5c7ee4b7921b31687b7037dd37c7f
Milena Hikari <milenahikari@MacBook-Air-de-Milena.local>
2025-04-17 16:15:02 -0300

    feat: add change order status and cancel order

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -5,7 +5,7 @@ import { Order } from '../../models/Order';
 
 export async function createOrder(req: Request, res: Response) {
   try {
-    const { table, products} = req.body;
+    const { table, products } = req.body;
 
     const order = await Order.create({ table, products });
 
--- src/router.ts
+++ src/router.ts
@@ -10,6 +10,8 @@ import { createProduct } from './app/useCases/products/createProduct';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createOrder } from './app/useCases/orders/createOrder';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { cancelOrder } from './app/useCases/orders/cancelOrder';
 
 export const router = Router();
 
@@ -39,10 +41,6 @@ router.get('/orders', listOrders);
 
 router.post('/orders', createOrder);
 
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.patch('/orders/:orderId', changeOrderStatus);
 
-router.delete('/orders/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.delete('/orders/:orderId', cancelOrder);
